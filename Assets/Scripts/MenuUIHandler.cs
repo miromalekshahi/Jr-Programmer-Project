27293f66001c2e35be90733a429eeb30ae87431b
Miro Malekshahi <44974673+miromalekshahi@users.noreply.github.com>
2023-02-15 19:42:11 +0200

    fixed exit button, added functionality to back to menu button

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
@@ -30,10 +34,12 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
-#if UNITY_EDIITOR
+#if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
         Application.Quit(); // original code to quit unity player
 #endif
     }
+
+
 }
