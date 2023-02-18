2a36eb851bb9f24b69ca343dec0468388bf9c95b
Miro Malekshahi <44974673+miromalekshahi@users.noreply.github.com>
2023-02-18 15:21:41 +0200

    added productivity unit

---
+++ Assets/Scripts/ProductivityUnit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductivityUnit : Unit
+{
+    protected override void BuildingInRange()
+    {
+        throw new System.NotImplementedException();
+    }
+}
